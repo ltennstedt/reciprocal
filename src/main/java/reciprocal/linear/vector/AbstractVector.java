f3f64b865ece0ed17c0f1ff5a81b4494964cac5d
Lars Tennstedt <27898583+ltennstedt@users.noreply.github.com>
2023-01-17 21:58:58 +0100

    More methods in base class for vectors added

--- src/main/java/reciprocal/linear/vector/AbstractVector.java
+++ src/main/java/reciprocal/linear/vector/AbstractVector.java
@@ -1,9 +1,9 @@
 package reciprocal.linear.vector;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
 import static org.apache.commons.lang3.Validate.noNullElements;
 
-import com.google.common.base.MoreObjects;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
@@ -17,13 +17,12 @@ import org.jetbrains.annotations.Nullable;
  *
  * @param <E> element
  * @param <Q> quotient of element
- * @param <V> this
+ * @param <V> vector
  * @param <N> norm
- * @param <A> absolute value of element
  * @since 0.0.1
  */
-public abstract class AbstractVector<E extends Number, Q extends Number, V extends AbstractVector<E, Q, V, N, A>,
-    N extends Number, A extends Number> implements Serializable {
+public abstract class AbstractVector<E extends Number, Q extends Number, V extends AbstractVector<E, Q, V, N>,
+    N extends Number> implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
@@ -91,6 +90,167 @@ public abstract class AbstractVector<E extends Number, Q extends Number, V exten
         return entries;
     }
 
+    /**
+     * Returns the sum of this and the summand
+     *
+     * @param summand summand
+     * @return sum
+     * @throws NullPointerException when {@code summand == null}
+     * @since 0.0.1
+     */
+    public abstract @NotNull V add(@NotNull V summand);
+
+    /**
+     * Returns the difference of this and the subtrahend
+     *
+     * @param subtrahend subtrahend
+     * @return difference
+     * @throws NullPointerException when {@code subtrahend == null}
+     * @since 0.0.1
+     */
+    public abstract @NotNull V subtract(@NotNull V subtrahend);
+
+    /**
+     * Returns the dot product of this and other
+     *
+     * @param other other
+     * @return dot product
+     * @throws NullPointerException when {@code other == null}
+     * @since 0.0.1
+     */
+    public abstract @NotNull V dotProduct(@NotNull V other);
+
+    /**
+     * Returns the scalar product pof this and the scalar
+     *
+     * @param scalar scalar
+     * @return scalar product
+     * @throws NullPointerException when {@code scalar == null}
+     * @since 0.0.1
+     */
+    public abstract @NotNull V scalarMultiply(@NotNull V scalar);
+
+    /**
+     * Returns the negated vector of this
+     *
+     * @return negated vector
+     * @since 0.0.1
+     */
+    public abstract @NotNull V negate();
+
+    /**
+     * Returns if this is orthogonal to other
+     *
+     * @param other other
+     * @return {@link Boolean}
+     * @throws NullPointerException when {@code other == null}
+     * @since 0.0.1
+     */
+    public abstract boolean orthogonalTo(@NotNull V other);
+
+    /**
+     * Returns the taxicab norm
+     *
+     * @return taxicab norm
+     * @since 0.0.1
+     */
+    public abstract @NotNull N taxicabNorm();
+
+    /**
+     * Returns the square of the euclidean norm
+     *
+     * @return square of the euclidean norm
+     * @since 0.0.1
+     */
+    public abstract @NotNull N euclideanNormPow2();
+
+    /**
+     * Returns the euclidean norm
+     *
+     * @return euclidean norm
+     * @since 0.0.1
+     */
+    public abstract @NotNull N euclideanNorm();
+
+    /**
+     * Returns the maximum norm
+     *
+     * @return max norm
+     * @since 0.0.1
+     */
+    public abstract @NotNull N maxNorm();
+
+    /**
+     * Returns the taxicab distance to other
+     *
+     * @param other other
+     * @return taxicab distance
+     * @throws NullPointerException when {@code other == null}
+     * @throws IllegalArgumentException when sizes are not equal
+     * @since 0.0.1
+     */
+    public final @NotNull N taxicabDistance(final @NotNull V other) {
+        requireNonNull(other, "other");
+        checkArgument(getSize() == other.getSize(), "equal sizes expected but %s != %s", getSize(), other.getSize());
+        return subtract(other).taxicabNorm();
+    }
+
+    /**
+     * Returns the euclidean distance to other
+     *
+     * @param other other
+     * @return euclidean distance
+     * @throws NullPointerException when {@code other == null}
+     * @throws IllegalArgumentException if sizes are not equal
+     * @since 0.0.1
+     */
+    public final @NotNull N euclideanDistance(final @NotNull V other) {
+        requireNonNull(other, "other");
+        checkArgument(getSize() == other.getSize(), "equal sizes expected but %s != %s", getSize(), other.getSize());
+        return subtract(other).euclideanNorm();
+    }
+
+    /**
+     * Returns the maximum distance to [other]
+     *
+     * @param other other
+     * @return max distance
+     * @throws NullPointerException when {@code other == null}
+     * @throws IllegalArgumentException if sizes are not equal
+     * @since 0.0.1
+     */
+    public final @NotNull N maxDistance(final @NotNull V other) {
+        requireNonNull(other, "other");
+        checkArgument(getSize() == other.getSize(), "equal sizes expected but %s != %s", getSize(), other.getSize());
+        return subtract(other).maxNorm();
+    }
+
+    /**
+     * Returns the element on the index
+     *
+     * @param index index
+     * @return element
+     * @throws IllegalArgumentException when {@code index < 1 || index > size}
+     * @since 0.0.1
+     */
+    public final @NotNull E get(final int index) {
+        checkArgument(index > 0 && index <= getSize(), "expected 0 < index <= %s but index = %s", getSize(), index);
+        return entries.get(index).element();
+    }
+
+    /**
+     * Returns if this contains element
+     *
+     * @param element element
+     * @return {@link Boolean}
+     * @throws NullPointerException when {@code element == null}
+     * @since 0.0.1
+     */
+    public final boolean contains(final @NotNull E element) {
+        requireNonNull(element, "element");
+        return entries.stream().map(VectorEntry::element).anyMatch(e -> e.equals(element));
+    }
+
     @Override
     public final int hashCode() {
         return Objects.hash(entries);
@@ -104,12 +264,12 @@ public abstract class AbstractVector<E extends Number, Q extends Number, V exten
         if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
-        final var other = (AbstractVector<?, ?, ?, ?, ?>) obj;
+        final var other = (AbstractVector<?, ?, ?, ?>) obj;
         return entries.equals(other.getEntries());
     }
 
     @Override
     public final @NotNull String toString() {
-        return MoreObjects.toStringHelper(this).add("entries", entries).toString();
+        return getClass().getSimpleName() + "{entries=" + entries + "}";
     }
 }
